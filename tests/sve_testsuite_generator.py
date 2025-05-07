a1ce74527b71e7804a67a9e0232231318218296b
Alex Puscas <alex.puscas@tum.de>
2025-05-07 17:55:32 +0200

    cleanup

--- tests/sve_testsuite_generator.py
+++ tests/sve_testsuite_generator.py
@@ -68,8 +68,6 @@ def make(kernels, arch):
                 # ceiling division
                 vn = -(bn // -v_len) if "sme" in arch else bn
                 vm = -(bm // -v_len)  
-                vk = -(bk // -v_len) # should come out to 1 for bk = 1, meaning no changes for sve
-                # if not ((vn + bk) * vm + vn * bk <= 32):
                 if not ((vm + vn) * bk <= 32):
                     print(f'Skipping block size {bm}x{bn} for {arch}')
                     continue
@@ -83,7 +81,7 @@ def make(kernels, arch):
             subprocess.STDOUT for stderr
             """
             try:
-                subprocess.check_output(arguments + additional_args, stderr=sys.stderr, env={**os.environ, "dDEBUG_SUBPROCESS": "1"})
+                subprocess.check_output(arguments + additional_args, stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
@@ -124,8 +122,6 @@ def make(kernels, arch):
                 # ceiling division
                 vn = -(bn // -v_len) if "sme" in arch else bn
                 vm = -(bm // -v_len)
-                vk = -(bk // -v_len) # should come out to 1 for bk = 1, meaning no changes for sve
-                # if not ((vn + bk) * vm + vn * bk <= 32):
                 if not ((vm + vn) * bk <= 32):
                     # print(f'Skipping block size {bm}x{bn} for {arch}')
                     continue
@@ -134,7 +130,6 @@ def make(kernels, arch):
 
             if isinstance(kern, SparseKernel) or isinstance(kern, SparseKernelS):
                 mtx = kern.mtx
-                print(os.path.abspath(mtx))
             else:
                 mtx = ""
             # for double precision: set prec to '' to conform to test_generator.function_definitions
@@ -143,11 +138,7 @@ def make(kernels, arch):
 
             setup_Atrans ="""posix_memalign(reinterpret_cast<void **>(&{p}Atrans), 64, {lda}*{ldbsparse}*sizeof({T}));
   transpose_matrix(std::get<0>({p}pointers), {p}Atrans, {lda}, {ldbsparse});
-  //printf("A:\\n");
-  //pretty_print({m}, {k}, {ldbsparse}, std::get<0>{p}(pointers));
-  //printf("{p}Atrans:\\n");
-  //pretty_print({k}, {m}, {lda}, {p}Atrans);
-  //printf("\\n");""".format(m=kern.m, k=kern.k, lda=kern.lda, ldbsparse=kern.k if sparse else kern.ldb, p=prec, 
+""".format(m=kern.m, k=kern.k, lda=kern.lda, ldbsparse=kern.k if sparse else kern.ldb, p=prec, 
                        T="float" if prec == f else "double") if has_matrix_ins else ""
             free_Atrans = "free({p}Atrans);".format(p=prec) if has_matrix_ins else ""
 
